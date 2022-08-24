9a17e0d72abba57bbfa8a76d7c858929c2b27e9a
Priscila Borges <priscila_borges87@hotmail.com>
2022-08-23 23:49:26 -0300

    Dados

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,15 @@
+let nome 
+    nome = "Priscila"
+let email 
+    email = "priscila_borges87@hotmail.com"
+let profissao 
+    profissao = "Analista de Processos"
+let age
+    age = "35"
+    let lugar 
+    lugar = "Bahamas"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(age)
+
